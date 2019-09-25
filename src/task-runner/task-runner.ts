d058a9ac53368402039fed4cc352b19eb6a3918b
Matthew Wilkes <tonicblue@gmail.com>
2019-09-25 15:32:03 +0100

    Testing

--- src/task-runner/task-runner.ts
+++ src/task-runner/task-runner.ts
@@ -33,17 +33,22 @@ export class TaskRunner {
   static renderer = new Renderer();
 
   static async run(tasks: TaskConfig[], context: TaskRunnerContext = new TaskRunnerContext(), root?: JQuery<HTMLElement>, renderOutput?: RenderConfig): Promise<any> {
+    console.log('About to run the following tasks', tasks);
     for (const task of tasks) {
       const taskModule = TaskRunner.taskModules.hasOwnProperty(task.type) ? TaskRunner.taskModules[task.type] : TaskRunner.taskModules.basic;
       const taskName = taskModule.moduleType + '-' + Math.floor(Math.random() * (999999 - 100000 + 1) ) + 100000;
 
+      console.log('Running task', taskName, task);
+
       try {
         const taskOutput = await taskModule.execute(context, task, root);
+        console.log('Task output', taskOutput);
         context.data[taskName] = taskOutput;
         if (typeof taskOutput === 'object' && taskOutput.hasOwnProperty('__halt') && taskOutput.__halt === true) {
           break;
         }
       } catch (err) {
+        console.error('Failed to run task', err);
         context.errors[taskName] = err;
         if (task.haltOnError) {
           break;
