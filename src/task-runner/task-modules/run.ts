e417a9a6f25b652ee33a7fe4c8aabc55ddac7076
Matthew Wilkes <tonicblue@gmail.com>
2019-09-30 16:32:48 +0100

    Testing

--- src/task-runner/task-modules/run.ts
+++ src/task-runner/task-modules/run.ts
@@ -6,15 +6,12 @@ import { Basic } from './basic';
 export class Run extends Basic<any> {
   async main(context: TaskRunnerContext, taskConfig: TaskConfig, config: RunConfig): Promise<any> {
     try {
-      const output = this.safeEval(config.code, config.useWindow ? window : context);
-      return output;
+      const func = (new Function(`with(this) { return ${config.code} }`));
+      const runContext = config.useWindow ? window : context;
+      return func.call(runContext);
     } catch(err) {
       console.error('Failed to safely evaluate Dom Manipulator rule', err);
     }
-  }
-
-  safeEval(fn: string, context: any) {
-    return Function('"use strict"; return (' + fn + ')')()(context);
   }
 }
 
