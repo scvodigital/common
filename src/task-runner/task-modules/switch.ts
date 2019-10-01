25799913578d5254c1543948ff2a4149ebd11d4d
Matthew Wilkes <tonicblue@gmail.com>
2019-10-01 09:37:20 +0100

    Testing

--- src/task-runner/task-modules/switch.ts
+++ src/task-runner/task-modules/switch.ts
@@ -14,12 +14,15 @@ export class Switch extends Basic<SwitchConfig> {
       throw new Error(`No branch called '${config.which}' or 'default' exists`);
     }
 
-    let output: any = {};
+    let output: any;
     if (branch.tasks) {
       output = await TaskRunner.run(branch.tasks, context, branch.renderOutput);
     }
 
     if (branch.halt) {
+      if (!output) {
+        output = {};
+      }
       output.__halt = true;
     }
 
